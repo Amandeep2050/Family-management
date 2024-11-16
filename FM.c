925a4a3a2b7da404cbe6e4dca5f2efbfff452a92
Amandeep2050 <amandeepramola638@gmail.com>
2024-11-16 19:53:09 +0530

    First update

--- FM.c
+++ FM.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
+#include<string.h>
 
-void Login();
-void Register();
+void Login(); // Login Page
+void Register();  // Register Page
+int string_equator(char str1[], char str2[]);
 
 int main(){
     int choice;
@@ -29,9 +31,56 @@ int main(){
 }
 
 void Login(){
-    printf("Coming Soon.............");
+    char inuser[100], outuser[100], check_user[100];
+    int pass, outpass;
+    printf("***************\n");
+    printf("=====Login=====\n");
+    FILE *fptr;
+    for(int j=0; ;j++){
+    fptr = fopen("userpass.txt", "r");
+    printf("Enter Username:- ");
+    scanf("%s", &inuser);
+    for(int i=0; ;i++){
+        fscanf(fptr, "%s", outuser);
+        if(string_equator(check_user, outuser)){
+            printf("Incorrect Username!!\n");
+            break;
+        }
+        if(string_equator(inuser, outuser)){
+            fscanf(fptr, "%d", &outpass);
+            
+            printf("Enter Password: ");
+            scanf("%d", &pass);
+            if(pass == outpass){
+                printf("Correct Password");
+                return;
+            }
+            else{
+                printf("Incorrect Password\nPlease retry\n");
+            }
+        }
+        strcpy(check_user, outuser);
+    }
+    fclose(fptr);
+    }
 }
 
 void Register(){
     printf("Coming Soon.............");
+}
+
+int string_equator(char str1[],char str2[]){
+    int str1_len = strlen(str1);
+    int str2_len = strlen(str2);
+    if(str1_len == str2_len){
+        for(int i=0;i<=strlen(str1);i++){
+            if(str1[i] != str2[i]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
